3aceaebeb7d5282005d1c89ccc5a411ce45c0c3c
Júlia Moura <julia.araujo.moura.2020@gmail.com>
2025-02-04 11:20:33 -0300

    mais atualizações
    no arquivo 04

--- 04-objetos-genericos.php
+++ 04-objetos-genericos.php
@@ -11,11 +11,34 @@
 
 <?php
 // criando uma instância chamada de uma classe genérica chamada stdClass (Standar class)
-$usuario = new stdClass() //instância ou objeto 
-?>    
+$usuario = new stdClass(); //instância ou objeto 
 
+//  Adicionando propriedades/atributos ao objeto
+$usuario -> nome = "Chapolin Colorado";
+$usuario -> idade = 25;
+$usuario -> email = "chapolin.colorado@gmail.com";
+$usuario -> telefone = ["11-21350300", "11-98888-7777"];  
+?>   
     <h2>Analisando a estrutura do objeto</h2>
     <pre><?=var_dump($usuario)?></pre>
+    <hr>
+
+    <h2>Saída de dados</h2>
+    <p>Nome: <?=$usuario-> nome?></p>
+    <p>Idade: <?=$usuario-> idade?>anos</p>
+    <p>Telefone: <?=$usuario-> telefones[1]?></p>
+
+    <hr>
+
+    <h2>Convertendo um objeto em um array associativo
+
+<?php
+$arrayUsuario = (array) $usuario;
+?>    
+    
+    <h3>Analisando o array gerado a partir de um obejto</h3>
+    <pre><?=var_dump($arrayUsuario)?></pre>
+
 </body>
 
 </html>
