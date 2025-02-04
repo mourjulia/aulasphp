caca21b16adcca56dc1e0227b0c3ec2fff80ed5e
Júlia Moura <julia.araujo.moura.2020@gmail.com>
2025-02-04 11:34:06 -0300

    criação do arquivo 05

---
+++ 05-condicionais.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Estruturas condicionais</h1>
+    <hr>
+
+    <h2>Simples</h2>    
+
+    <h2>composta</h2>
+
+    <h2>Encadeadas</h2>
+
+
+
+</body>
+</html>
