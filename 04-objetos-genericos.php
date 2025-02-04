328abb1969b733c908fc9a12c25c720e08dfee3e
Júlia Moura <julia.araujo.moura.2020@gmail.com>
2025-02-04 11:01:54 -0300

    Criação do exemplo 04 sobre objetos
    objetos genéricos

---
+++ 04-objetos-genericos.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Objetos</title>
+</head>
+<body>
+    <h1>Trabalhando com Objetos genéricos</h1>
+    <hr>
+
+<?php
+// criando uma instância chamada de uma classe genérica chamada stdClass (Standar class)
+$usuario = new stdClass() //instância ou objeto 
+?>    
+
+    <h2>Analisando a estrutura do objeto</h2>
+    <pre><?=var_dump($usuario)?></pre>
+</body>
+
+</html>
