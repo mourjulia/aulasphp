b6439dafd8fd8ad17311e6f7d967ae81cf5a6817
Júlia Moura <julia.araujo.moura.2020@gmail.com>
2025-02-04 11:42:10 -0300

    Atualizações das condicionais
    condicionais simples

--- 05-condicionais.php
+++ 05-condicionais.php
@@ -9,7 +9,17 @@
     <h1>Estruturas condicionais</h1>
     <hr>
 
-    <h2>Simples</h2>    
+    <h2>Simples usando <code>if</code></h2>   
+    <h3><code>if</code></h3>
+<?php
+$numero = 5;
+if($numero > 1){
+    echo "<p>$numero é maior que 1</p>";
+}
+
+if ($numero > 1) echo "<p>$numero é maior que 1<?p>"
+
+?>    
 
     <h2>composta</h2>
 
