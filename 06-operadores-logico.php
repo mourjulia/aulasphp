e4d1d7b20d066638705a3239afd71b26971930fb
Júlia Moura <julia.araujo.moura.2020@gmail.com>
2025-02-06 08:25:22 -0300

    criação do arquivo 06
    trabalhando com operadores lógicos, ou e e não

---
+++ 06-operadores-logico.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Operadores lógicos </title>
+</head>
+<body>
+    <h1>relembranco os Operadores Lógicos </h1>
+    <hr>
+
+    <h2>&& (E/AND)</h2>
+
+    <h2>|| (OU/OR)</h2>
+
+    <h2>! (NÃO/NOT)</h2>
+
+
+</body>
+</html>
