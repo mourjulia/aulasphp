d14df9812c3ce2031e61ef66234973268e334946
Júlia Moura <julia.araujo.moura.2020@gmail.com>
2025-02-06 09:34:11 -0300

    criando arquivo 07
    conceitos de loops

---
+++ 07-loops.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Loops</title>
+</head>
+<body>
+    <h1>Trabalhando com comandos/laços de repetição</h1>
+    <hr>
+
+    <h2>WHILE (ENQUANTO)</h2>
+    <p>Executa ações <b>enquanto a condição for verdadeira</b>. Se a condição
+    inicialmente não é verdadeira, o loop não funciona nenhuma vez.</p>
+
+    <h2>DO/WHILE (PARECIDO COM REPITA)</h2>
+    <p>Repete <b>pelo menos uma vez</b> as ações e, caso a condição continue
+    verdadeira, segue fazendo outros ciclos de repetição até a condição se tornar
+    falsa.</p>
+
+    <h2>FOR (PARA)</h2>
+    <p>Executa ações por uma <b>quantidade determinada</b> de vezes.</p>
+</body>
+</html>
