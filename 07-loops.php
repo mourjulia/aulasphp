640f6eea00b358ab175f6bdd54f4bee50a11d5c5
Júlia Moura <julia.araujo.moura.2020@gmail.com>
2025-02-06 09:48:00 -0300

    Adicionando exemplo de DO/WHILE

--- 07-loops.php
+++ 07-loops.php
@@ -24,10 +24,25 @@ while ($i <= 3) {
 }
 ?>
 
+    <hr>
+
     <h2>DO/WHILE (PARECIDO COM REPITA)</h2>
     <p>Repete <b>pelo menos uma vez</b> as ações e, caso a condição continue
     verdadeira, segue fazendo outros ciclos de repetição até a condição se tornar
     falsa.</p>
+
+<?php
+$j = 1;
+do {
+?>
+        <div>
+            <h3>Título......</h3>
+            <p>Texto qualquer</p>
+        </div>
+<?php
+    $j++;
+} while($j <= 3);
+?>
 
     <h2>FOR (PARA)</h2>
     <p>Executa ações por uma <b>quantidade determinada</b> de vezes.</p>
