fe3990cdfb83e77c628babd3cca96befda0beb73
Júlia Moura <julia.araujo.moura.2020@gmail.com>
2025-02-14 08:53:23 -0300

    incluindo o cabeçalho com include""

--- 11-site/duvidas.php
+++ 11-site/duvidas.php
@@ -1,26 +1,4 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
-</head>
-
-<body>
-    <div class="container">
-        <header>
-            <h1>Página de Dúvidas - Site de PHP</h1>
-            <nav>
-                <a href="index.php">HOME</a>
-                <a href="cursos.php">Cursos</a>
-                <a href="duvidas.php">Dúvidas</a>
-            </nav>
-        </header>
-
-        <main>
+<?php include "cabecalho.php" ?>
             <h2>Página de Dúvidas</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, officia.
             Velit neque corporis vitae nam harum qui repudiandae, optio molestias ex id,
