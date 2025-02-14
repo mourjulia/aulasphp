088fb94b2f69c0dfd2da97ed11c8edd23abf7003
Júlia Moura <julia.araujo.moura.2020@gmail.com>
2025-02-14 08:33:38 -0300

    criação de site
    Página Home e Cursos

---
+++ 11-site/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <div class="container">
+        <header>
+            <h1>Site de PHP</h1>
+            <nav>
+                <a href="index.php">HOME</a>
+                <a href="cursos.php">Cursos</a>
+                <a href="duvidas.php">Dúvidas</a>
+            </nav>
+        </header>
+
+        <main>
+            <h2>Bem-vindo ao site Exemplo</h2>
+            <p>Esta é a primeira parte do nosso site.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Recusandae magni unde obcaecati odit molestiae placeat cumque
+            quos quae, et pariatur maiores nesciunt, explicabo iusto corporis,
+            eos soluta delectus tempora iste.</p>
+        </main>
+        <footer>
+            <p>Site criado para curso Téc. Informática para Internet</p>
+        </footer>
+
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
