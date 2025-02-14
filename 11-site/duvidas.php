afc4cfc9ea3b7119afda73b85d466ba5e813df86
Júlia Moura <julia.araujo.moura.2020@gmail.com>
2025-02-14 09:00:10 -0300

    adicionando o cabeçalho com include

--- 11-site/duvidas.php
+++ 11-site/duvidas.php
@@ -14,17 +14,4 @@
                 <summary>Os cursos são gratuitos?</summary>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
             </details>
-        </main>
-        
-        <footer>
-            <p>Site criado para curso Téc. Informática para Internet</p>
-        </footer>
-
-    </div>
-
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</body>
-
-</html>
+<?php include "rodape.php" ?>
--- 11-site/index.php
+++ 11-site/index.php
@@ -1,21 +1,9 @@
 <?php include "cabecalho.php" ?>
-""
             <h2>Bem-vindo ao site Exemplo</h2>
             <p>Esta é a primeira parte do nosso site.</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
             Recusandae magni unde obcaecati odit molestiae placeat cumque
             quos quae, et pariatur maiores nesciunt, explicabo iusto corporis,
             eos soluta delectus tempora iste.</p>
-        </main>
-        <footer>
-            <p>Site criado para curso Téc. Informática para Internet</p>
-        </footer>
 
-    </div>
-
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</body>
-
-</html>
+<?php include "rodape.php" ?>
