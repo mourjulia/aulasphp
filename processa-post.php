3c3c715a7d505d11bea0bc305884068a38f0f8eb
Júlia Moura <julia.araujo.moura.2020@gmail.com>
2025-02-17 08:47:56 -0300

    atualização

--- processa-post.php
+++ processa-post.php
@@ -1,44 +1,43 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP</title>
+    <title>PHP - Processamento de dados</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
 </head>
+
 <body>
-   <div class="container">
+    <div class="container">
         <h1>Processamento usando POST</h1>
         <hr>
-        <pre><?=var_dump($_POST)?></pre>
-        
-   </div> 
 
-   <form action="processa-post.php" method="get" >
-    <div class="mb-3">
-        <label for="nome" class="form-label">Nome:</label>
-        <input class="form-control" type="text" name="nome" id="nome">
+
+        <?php
+        //  Capturando os dados transmitidos
+        $nome = $_POST["nome"];
+        $email = $_POST["email"];
+        $idade = $_POST["idade"];
+        $mensagem = $_POST["mensagem"];
+
+        ?>
+
+        <h2>Dados:</h2>
+        <ul>
+            <li>Nome:<?= $nome ?></li>
+            <li>Email:<?= $email ?></li>
+            <li>Idade:<?= $idade ?></li>
+            <li>Mensagem:<?= $mensagem ?></li>
+        </ul>
+
     </div>
-    <div class="mb-3">
-        <label for="email">E-mail:</label>
-        <input class="form-control" type="email" name="email" id="email">
-    </div>
-    <div class="mb-3">
-        <label for="idade">Idade:</label>
-        <input class="form-control" type="number" name="idade" id="idade" min="1" max="100">
-    </div>
-
-    <div class="mb-3">
-        <label for="mensagem">Mensagem:</label>
-
-        <textarea class="form-control" name="mensagem" id="mensagem" cols="30" rows="6"></textarea>
-    </div>
-    <button class="btn btn-primary" type="submit">Enviar dados</button>
-</form>
 
 
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
+
 </html>
